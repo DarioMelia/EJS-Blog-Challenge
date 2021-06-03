b1dd58e19dbe731805b44a4421dff4826e135f57
DarioMeliaBasus <dario@meliabasus.com>
2021-06-03 11:27:39 +0200

    Add local database and post storage functionality

--- app.js
+++ app.js
@@ -3,75 +3,110 @@
 const express = require("express");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-const posts =  [];
 
+
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Post needs a title"]
+  },
+  text: {
+    type: String,
+    required: [true, "Post needs content"]
+  }
+});
+
+const Post = mongoose.model("post", postSchema);
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
-app.get("/", function(req, res){
-  res.render("home", {
-    homeContent: homeStartingContent,
-    posts: posts,
-    
-  });
+app.get("/", function(req, res) {
+  Post.find(function(err, posts) {
+    if (err) {
+      console.error(err)
+    }
+    res.render("home", {
+      homeContent: homeStartingContent,
+      posts: posts,
 
+    });
+  })
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function(req, res) {
   res.render("about", {
     aboutContent: aboutContent
   })
 });
 
-app.get("/contact", function(req, res){
+app.get("/contact", function(req, res) {
   res.render("contact", {
     contactContent: contactContent
   })
 });
 
-app.get("/compose", function(req,res){
+app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:title", function(req, res){
-   const urlTitle = _.lowerCase(req.params.title);
+app.get("/posts/:postId", function(req, res) {
+  const urlId = req.params.postId;
 
-  posts.forEach(function(post){
 
-    let postTitleLowCase = _.lowerCase(post.title);
 
-    if(postTitleLowCase === urlTitle){
-
-      res.render("post", {
-        post:post
-
-      })
+  Post.find(function(err, posts) {
+    if (err) {
+      console.error(err)
     }
-  });
+
+    posts.forEach(post => {
+
+      if (post._id.toString() === urlId) {
+
+        res.render("post", {
+          post: post
+
+        })
+      }
+    });
+
+  })
 
 });
 
 
 
-app.post("/compose", function(req,res){
+app.post("/compose", async function(req, res) {
   const postTitle = req.body.postTitle;
   const text = req.body.postText;
-  const post = {
+
+  const post = new Post({
     title: postTitle,
-    kebabTitle: _.kebabCase(postTitle),
-    body: text
-  }
-  posts.push(post);
+    text: text
+  })
+  await post.save();
+
   res.redirect("/");
 })
 
